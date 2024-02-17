c01dff925d5b7144abed5493685fb041f499ae51
Loc Nguyen <vinhloc30796@gmail.com>
2024-02-17 15:57:54 +0700

    Call function injectSpeedInsights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,12 @@
 	import { page } from '$app/stores';
 	import { webVitals } from '$lib/vitals';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import Header from './Header.svelte';
 	import './styles.css';
 	
 	inject({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 
 	/** @type {import('./$types').LayoutServerData} */
 	export let data;
