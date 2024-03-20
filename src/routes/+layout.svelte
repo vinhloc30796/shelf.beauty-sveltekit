0c30405c7ea76bb920af328af122fecd3b94312d
Loc Nguyen <vinhloc30796@gmail.com>
2024-03-20 21:52:17 +0700

    Inject Vercel Speed Insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,10 @@
 	// Vercel Analytics
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 	inject({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights()
 
 	// Dark mode
 	import { ModeWatcher } from 'mode-watcher';
