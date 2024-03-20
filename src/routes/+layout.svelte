8a4a12c8200a5e8aa326e7441be3669c544ebed4
Loc Nguyen <vinhloc30796@gmail.com>
2024-03-20 16:26:49 +0700

    Add button for directions (#6)
    * Add button for directions
    
    * Specify href & target params for buttons
    
    * Fix width for carousel on smaller screens
    
    * Add gtag
    
    * Add text to FB Messenger link
    
    * Add Google Tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,8 @@
 	// Dark mode
 	import { ModeWatcher } from 'mode-watcher';
 
+	// Typography
+	import P from '$lib/components/typography/p.svelte';
 	// Header component
 	import Header from './Header.svelte';
 
@@ -27,13 +29,14 @@
 		class="flex w-full min-h-16 items-center justify-center bg-gray-100 dark:bg-gray-900 mt-8"
 		id="footer"
 	>
-		<p>
+		<P class="text-center text-sm">
 			Ghé <a
-				href="https://m.me/shelfbeautystudio"
+				class="text-blue-500 hover:underline"
+				href="https://m.me/shelfbeautystudio?text=Cho+mình+xin+đặt+hẹn+làm+nail+với+ạ"
 				title="Shelf Beauty Studio trên Facebook"
 				referrerpolicy="origin"
 				target="_blank">Shelf Beauty Studio trên Facebook Messenger</a
 			> để đặt lịch hẹn
-		</p>
+		</P>
 	</footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,14 @@
 	import image_3 from '$lib/images/operations/3.jpg';
 	import image_4 from '$lib/images/operations/4.jpg';
 	import image_5 from '$lib/images/operations/5.jpg';
+	// Icons
+	import { Calendar, ThickArrowRight } from 'svelte-radix';
 	// Typography
 	import H1 from '$lib/components/typography/h1.svelte';
 	import H2 from '$lib/components/typography/h2.svelte';
 	import P from '$lib/components/typography/p.svelte';
 	// Components
+	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import { type CarouselAPI } from '$lib/components/ui/carousel/context.js';
 	import * as Carousel from '$lib/components/ui/carousel/index.js';
@@ -30,36 +33,87 @@
 			current = api.selectedScrollSnap() + 1;
 		});
 	}
+
+	// Event snippet for Get directions conversion page
+	// In your html page, add the snippet and call gtag_report_conversion
+	// - when someone clicks on the chosen link or button.
+	$: gtag_report_conversion = (url: string) => {
+		// @ts-ignore
+		window.gtag('event', 'conversion', {
+			send_to: 'AW-11464920859/XeK7CPaZ2YUZEJue89oq',
+			event_callback: () => {
+				if (url) {
+					window.location;
+				}
+			}
+		});
+		// console.log('gtag_report_conversion: sent to Google Analytics');
+		return false;
+	};
 </script>
 
 <svelte:head>
 	<title>Shelf Beauty Studio - Trang Chủ</title>
 	<meta name="description" content="Shelf Beauty Studio - Trang Chủ" />
+
 </svelte:head>
 
-<div class="flex flex-col min-h-[calc(100vh-12rem)]">
-	<div class="grid grid-cols-1 lg:grid-cols-3 gap-4 h-auto" id="hero">
-		<section class="flex flex-col mx-16 items-center justify-center col-span-1" id="hero-desc">
-			<span class="w-full flex">
+<div class="flex flex-col min-h-[calc(100vh-12rem)] max-w-1/2">
+	<div class="grid grid-cols-1 xl:grid-cols-3 gap-4 h-auto" id="hero">
+		<section class="flex flex-col mx-16 justify-center items-center col-span-1" id="hero-desc">
+			<!-- Logo -->
+			<span class="flex w-full my-4">
 				<picture>
 					<img src={shelf_dark} alt="Shelf Beauty Studio - Welcome" class="dark:hidden" />
 					<img src={shelf_light} alt="Shelf Beauty Studio - Welcome" class="hidden dark:block" />
 				</picture>
 			</span>
-			<H2 class="mt-4">Trải nghiệm làm đẹp tại Đà Lạt</H2>
-			<P>
-				Shelf Beauty Studio là một không gian làm đẹp chuyên nghiệp, nơi bạn có thể tìm thấy sự yên
-				bình và thoải mái.
-			</P>
+			<!-- Description -->
+			<div class="my-4">
+				<H2 class="mt-4">Trải nghiệm làm đẹp tại Đà Lạt</H2>
+				<P>
+					Shelf Beauty Studio là một không gian làm đẹp chuyên nghiệp, nơi bạn có thể tìm thấy sự
+					yên bình và thoải mái.
+				</P>
+			</div>
+			<!-- Buttons -->
+			<div class="flex flex-col sm:flex-row w-full justify-items-center items-center my-4">
+				<div class="flex w-full mx-1 my-4 justify-self-center justify-center items-center">
+					<Button
+						variant="default"
+						class="h-12 w-36"
+						href="https://www.google.com/maps/dir/?api=1&destination=shelf+beauty+studio,+Yersin,+Ph%C6%B0%E1%BB%9Dng+10,+Dalat,+Lam+Dong&destination_place_id=ChIJHydiEXkTcTERBlm-4kPGIWk"
+						title="Shelf Beauty Studio trên Google Maps"
+						referrerpolicy="origin"
+						target="_blank"
+						on:click={() => gtag_report_conversion('https://shelf.beauty/directions')}
+					>
+						<ThickArrowRight class="mr-2" />
+						Tìm đường
+					</Button>
+				</div>
+				<div class="flex w-full mx-1 my-4 justify-self-center justify-center items-center">
+					<Button
+						variant="secondary"
+						class="h-12 w-36"
+						href="https://m.me/shelfbeautystudio?text=Cho+mình+xin+đặt+hẹn+làm+nail+với+ạ"
+						title="Shelf Beauty Studio trên Facebook"
+						referrerpolicy="origin"
+						target="_blank"
+					>
+						<Calendar class="mr-2" />
+						Đặt hẹn
+					</Button>
+				</div>
+			</div>
 		</section>
-		<section class="flex flex-col w-5/6 mx-16 items-center justify-center col-span-2">
+		<section class="flex flex-col mx-16 items-center justify-center col-span-1 xl:col-span-2">
 			<Carousel.Root
 				bind:api
 				orientation="horizontal"
 				opts={{
 					align: 'start',
 					dragFree: true,
-					containScroll: 'trimSnaps',
 					skipSnaps: false
 				}}
 				plugins={[
@@ -69,6 +123,7 @@
 						waitForTransition: true
 					})
 				]}
+				class="w-full"
 			>
 				<Carousel.Content class="flex flex-row items-center justify-center">
 					{#each Array(images.length - 1) as _}
