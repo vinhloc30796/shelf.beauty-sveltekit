7d671c30b3cc054adeb7abf37bad7e78ae06dac0
Loc Nguyen <vinhloc30796@gmail.com>
2024-05-10 16:20:36 +0700

    Use OAuth for internal apps to bypass 7d refresh

--- src/lib/server/ggMyBusinessApi.ts
+++ src/lib/server/ggMyBusinessApi.ts
@@ -6,53 +6,30 @@ import {
 	GCP_OAUTH_CLIENT_SECRET,
 	GCP_OAUTH_REDIRECT_URL,
 	GCP_OAUTH_REFRESH_TOKEN,
-	GCP_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-	GCP_SERVICE_ACCOUNT_PRIVATE_KEY,
-	GCP_SERVICE_ACCOUNT_CLIENT_EMAIL,
-	GCP_SERVICE_ACCOUNT_CLIENT_ID,
 	GMB_LOCATION_ID
 } from '$env/static/private';
 import { Auth, google } from 'googleapis';
 
 // const SCOPES = ['https://www.googleapis.com/auth/business.manage'];
 async function tryAuth(): Promise<Auth.GoogleAuth | Auth.OAuth2Client | undefined> {
-	// Try with Service Account
-	try {
-        const serviceAcctClient = new google.auth.GoogleAuth({
-            scopes: ['https://www.googleapis.com/auth/business.manage'],
-            credentials: {
-                client_email: GCP_SERVICE_ACCOUNT_CLIENT_EMAIL,
-                client_id: GCP_SERVICE_ACCOUNT_CLIENT_ID,
-                private_key: GCP_SERVICE_ACCOUNT_PRIVATE_KEY,
-                private_key_id: GCP_SERVICE_ACCOUNT_PRIVATE_KEY_ID
-            }
-        });
-        const serviceAuthCientResult = await serviceAcctClient.getClient();
-        console.log(`serviceAcctClient result: success, `, serviceAuthCientResult);
-        google.options({ auth: serviceAcctClient });
-        return serviceAcctClient;
-    } catch (error) {
-        console.error(`serviceAcctClient error: `, error);
-    }
-
 	// Try with OAuth2
-    try {
-        const oauth2Client = new google.auth.OAuth2(
-            GCP_OAUTH_CLIENT_ID,
-            GCP_OAUTH_CLIENT_SECRET,
-            GCP_OAUTH_REDIRECT_URL
-        );
-        oauth2Client.setCredentials({ refresh_token: GCP_OAUTH_REFRESH_TOKEN });
-        const token = await oauth2Client.getAccessToken();
-        console.log(`oauth2Client result: success, `, token);
-        google.options({ auth: oauth2Client });
-        return oauth2Client;
-    } catch (error) {
-        console.error(`oauth2Client error: `, error);
-    }
+	try {
+		const oauth2Client = new google.auth.OAuth2(
+			GCP_OAUTH_CLIENT_ID,
+			GCP_OAUTH_CLIENT_SECRET,
+			GCP_OAUTH_REDIRECT_URL
+		);
+		oauth2Client.setCredentials({ refresh_token: GCP_OAUTH_REFRESH_TOKEN });
+		const token = await oauth2Client.getAccessToken();
+		console.log(`oauth2Client result: success, `, token);
+		google.options({ auth: oauth2Client });
+		return oauth2Client;
+	} catch (error) {
+		console.error(`oauth2Client error: `, error);
+	}
 
 	// Return undefined if both methods fail
-    return undefined;
+	return undefined;
 }
 
 const auth = await tryAuth();
