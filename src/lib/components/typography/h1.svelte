b2287cd6b62fc8e458048f344e595b510a90245f
Loc Nguyen <vinhloc30796@gmail.com>
2024-05-26 16:56:34 +0700

    Add margin bottom to H1

--- src/lib/components/typography/h1.svelte
+++ src/lib/components/typography/h1.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <h1
-	class={cn('scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl', className)}
+	class={cn('scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-12', className)}
 	{...$$restProps}
 >
 	<slot />
